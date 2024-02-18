9db864b5d108cf8d260d8857258e210fbda96754
salimsayed <salemsayed981@gmail.com>
2024-02-18 02:49:24 +0200

    UPD Create Category v1

--- src/component/pages/Category/Category.tsx
+++ src/component/pages/Category/Category.tsx
@@ -78,7 +78,7 @@ export default function Category() {
           dataSource={categories ? categories : dataSource}
           columns={columns}
         />
-        ;
+         
       </Center>
     </AdminLayout>
   );
